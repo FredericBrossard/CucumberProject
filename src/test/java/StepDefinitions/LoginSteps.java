df3d7011184dc283bcb155845258d113e2f2cd82
Frederic Brossard <frederic.brossard.pf1@gmail.com>
2021-06-30 22:40:41 +0200

    project save 1 first

---
+++ src/test/java/StepDefinitions/LoginSteps.java
@@ -0,0 +1,37 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginSteps {
+
+	@Given("today is Sunday")
+	public void today_is_Sunday() {
+		// Write code here that turns the phrase above into concrete actions
+		//throw new io.cucumber.java.PendingException();
+
+		System.out.println("today is Sunday");
+	}
+
+	@When("some other precondition")
+	public void some_other_precondition( ) {
+		// Write code here that turns the phrase above into concrete actions
+		//throw new io.cucumber.java.PendingException();
+		System.out.println("some other precondition");
+	}
+	@And("I complete action")
+	public void I_complete_action( ) {
+		// Write code here that turns the phrase above into concrete actions
+		//throw new io.cucumber.java.PendingException();
+		System.out.println("complete action");
+	}
+	@Then("I should be told {string}")
+	public void i_should_be_told(String string) {
+		// Write code here that turns the phrase above into concrete actions
+		//throw new io.cucumber.java.PendingException();
+		System.out.println("today is Sunday");
+	}
+
+}
